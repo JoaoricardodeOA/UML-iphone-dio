5a664dcb254e755a6ef3aceca0879b50b1db8544
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2024-06-16 14:53:15 -0300

    Feito UML em Java.

---
+++ src/umliphone/UMLIphone.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package umliphone;
+
+/**
+ *
+ * @author joaor
+ */
+public class UMLIphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
